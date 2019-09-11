2bc68ab98b214fa1c7e5c74b685c98f5f39c3342
ikun <ikunal03@gmail.com>
2019-09-11 13:44:45 -0400

    Added the framework page

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+//Just some code for testing purpose
+// let myHeading = document.querySelector("h1");
+// myHeading.textContent = "Hey Hello! Whatsup"
+// let myImage = document.getElementById("img1");
+//
+// myImage.onclick = function () {
+//     let mySrc = myImage.getAttribute('src');
+//     if (mySrc === 'images/scrimg.png') {
+//         myImage.setAttribute('src', 'images/spriral.jpg');
+//     } else {
+//         myImage.setAttribute('src', 'images/scrimg.png');
+//     }
+// }
