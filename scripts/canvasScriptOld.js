7bea3788fb82525383a7f6ca94408167aaa2c381
Kunal <ikunal03@gmail.com>
2019-09-15 02:33:10 -0400

    Working with the camera matrix and flipped around F

--- scripts/canvasScriptOld.js
+++ scripts/canvasScriptOld.js
@@ -298,8 +298,15 @@ function main() {
     drawScene();
 
     function drawScene() {
+        //For the camera
+        var numFs = 5;
+        var radius = 200;
+
+        //For resizing canvas
         resize(gl.canvas);
         gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
+        //For proper 3D
         gl.enable(gl.CULL_FACE);
         gl.enable(gl.DEPTH_TEST);
         // Tell WebGL how to convert from clip space to pixels
@@ -333,157 +340,200 @@ function main() {
         let fieldOfViewRadians = degToRad(50);
         var matrix = m4.perspective(fieldOfViewRadians, aspect, zNear, zFar);
 
-        matrix = m4.translate(matrix, translation[0], translation[1], translation[2]);
-        matrix = m4.xRotate(matrix, rotation[0]);
-        matrix = m4.yRotate(matrix, rotation[1]);
-        matrix = m4.zRotate(matrix, rotation[2]);
-        matrix = m4.scale(matrix, scale[0], scale[1], scale[2]);
+        //This was the code that was used for the original 'F'. Check next code block for camera stuff
+        // matrix = m4.translate(matrix, translation[0], translation[1], translation[2]);
+        // matrix = m4.xRotate(matrix, rotation[0]);
+        // matrix = m4.yRotate(matrix, rotation[1]);
+        // matrix = m4.zRotate(matrix, rotation[2]);
+        // matrix = m4.scale(matrix, scale[0], scale[1], scale[2]);
+        //
+        // // Set the matrix.
+        // gl.uniformMatrix4fv(matrixLocation, false, matrix);
+        //
+        // // Draw the geometry.
+        // var primitiveType = gl.TRIANGLES;
+        // var offset = 0;
+        // var count = 16 * 6;
+        // gl.drawArrays(primitiveType, offset, count);
 
-        // Set the matrix.
-        gl.uniformMatrix4fv(matrixLocation, false, matrix);
-
-        // Draw the geometry.
-        var primitiveType = gl.TRIANGLES;
-        var offset = 0;
-        var count = 16 * 6;
-        gl.drawArrays(primitiveType, offset, count);
+        //This are the changes made for the camera
+        var cameraMatrix = m4.yRotation(degToRad(0));
+        cameraMatrix = m4.translate(cameraMatrix, 0, 0, radius * 1.5);
+        //Make a view matrix from the camera matrix
+        var viewMatrix = m4.inverse(cameraMatrix);
+        //move the projection space to view space(the space in front of the camera)
+        var viewProjectionMatrix = m4.multiply(matrix, viewMatrix);
+        //Draw 'F's in a circle
+        for (var ii = 0; ii < numFs; ++ii) {
+            var angle = ii * Math.PI * 2 / numFs;
+            var x = Math.cos(angle) * radius;
+            var z = Math.sin(angle) * radius;
+            // add in the translation for this F
+            var translationMatrix = m4.translate(viewProjectionMatrix, x, 0, z);
+            // Set the matrix.
+            gl.uniformMatrix4fv(matrixLocation, false, translationMatrix);
+            // Draw the geometry.
+            var primitiveType = gl.TRIANGLES;
+            var offset = 0;
+            var count = 16 * 6;
+            gl.drawArrays(primitiveType, offset, count);
+        }
     }
 }
 
 // Fill the current ARRAY_BUFFER buffer
 // with the values that define a letter 'F'.
 function setGeometry(gl) {
+    let fPixelArray = new Float32Array([
+        // left column front
+        0, 0, 0,
+        0, 150, 0,
+        30, 0, 0,
+        0, 150, 0,
+        30, 150, 0,
+        30, 0, 0,
+
+        // top rung front
+        30, 0, 0,
+        30, 30, 0,
+        100, 0, 0,
+        30, 30, 0,
+        100, 30, 0,
+        100, 0, 0,
+
+        // middle rung front
+        30, 60, 0,
+        30, 90, 0,
+        67, 60, 0,
+        30, 90, 0,
+        67, 90, 0,
+        67, 60, 0,
+
+        // left column back
+        0, 0, 30,
+        30, 0, 30,
+        0, 150, 30,
+        0, 150, 30,
+        30, 0, 30,
+        30, 150, 30,
+
+        // top rung back
+        30, 0, 30,
+        100, 0, 30,
+        30, 30, 30,
+        30, 30, 30,
+        100, 0, 30,
+        100, 30, 30,
+
+        // middle rung back
+        30, 60, 30,
+        67, 60, 30,
+        30, 90, 30,
+        30, 90, 30,
+        67, 60, 30,
+        67, 90, 30,
+
+        // top
+        0, 0, 0,
+        100, 0, 0,
+        100, 0, 30,
+        0, 0, 0,
+        100, 0, 30,
+        0, 0, 30,
+
+        // top rung right
+        100, 0, 0,
+        100, 30, 0,
+        100, 30, 30,
+        100, 0, 0,
+        100, 30, 30,
+        100, 0, 30,
+
+        // under top rung
+        30, 30, 0,
+        30, 30, 30,
+        100, 30, 30,
+        30, 30, 0,
+        100, 30, 30,
+        100, 30, 0,
+
+        // between top rung and middle
+        30, 30, 0,
+        30, 60, 30,
+        30, 30, 30,
+        30, 30, 0,
+        30, 60, 0,
+        30, 60, 30,
+
+        // top of middle rung
+        30, 60, 0,
+        67, 60, 30,
+        30, 60, 30,
+        30, 60, 0,
+        67, 60, 0,
+        67, 60, 30,
+
+        // right of middle rung
+        67, 60, 0,
+        67, 90, 30,
+        67, 60, 30,
+        67, 60, 0,
+        67, 90, 0,
+        67, 90, 30,
+
+        // bottom of middle rung.
+        30, 90, 0,
+        30, 90, 30,
+        67, 90, 30,
+        30, 90, 0,
+        67, 90, 30,
+        67, 90, 0,
+
+        // right of bottom
+        30, 90, 0,
+        30, 150, 30,
+        30, 90, 30,
+        30, 90, 0,
+        30, 150, 0,
+        30, 150, 30,
+
+        // bottom
+        0, 150, 0,
+        0, 150, 30,
+        30, 150, 30,
+        0, 150, 0,
+        30, 150, 30,
+        30, 150, 0,
+
+        // left side
+        0, 0, 0,
+        0, 0, 30,
+        0, 150, 30,
+        0, 0, 0,
+        0, 150, 30,
+        0, 150, 0,
+    ]);
+
+    // Center the F around the origin and Flip it around. We do this because
+    // we're in 3D now with and +Y is up where as before when we started with 2D
+    // we had +Y as down.
+
+    // We could do by changing all the values above but I'm lazy.
+    // We could also do it with a matrix at draw time but you should
+    // never do stuff at draw time if you can do it at init time.
+    var matrix = m4.xRotation(Math.PI);
+    matrix = m4.translate(matrix, -50, -75, -15);
+
+    for (var ii = 0; ii < fPixelArray.length; ii += 3) {
+        var vector = m4.transformVector(matrix, [fPixelArray[ii + 0], fPixelArray[ii + 1], fPixelArray[ii + 2], 1]);
+        fPixelArray[ii + 0] = vector[0];
+        fPixelArray[ii + 1] = vector[1];
+        fPixelArray[ii + 2] = vector[2];
+    }
+
     gl.bufferData(
         gl.ARRAY_BUFFER,
-        new Float32Array([
-            // left column front
-            0, 0, 0,
-            0, 150, 0,
-            30, 0, 0,
-            0, 150, 0,
-            30, 150, 0,
-            30, 0, 0,
-
-            // top rung front
-            30, 0, 0,
-            30, 30, 0,
-            100, 0, 0,
-            30, 30, 0,
-            100, 30, 0,
-            100, 0, 0,
-
-            // middle rung front
-            30, 60, 0,
-            30, 90, 0,
-            67, 60, 0,
-            30, 90, 0,
-            67, 90, 0,
-            67, 60, 0,
-
-            // left column back
-            0, 0, 30,
-            30, 0, 30,
-            0, 150, 30,
-            0, 150, 30,
-            30, 0, 30,
-            30, 150, 30,
-
-            // top rung back
-            30, 0, 30,
-            100, 0, 30,
-            30, 30, 30,
-            30, 30, 30,
-            100, 0, 30,
-            100, 30, 30,
-
-            // middle rung back
-            30, 60, 30,
-            67, 60, 30,
-            30, 90, 30,
-            30, 90, 30,
-            67, 60, 30,
-            67, 90, 30,
-
-            // top
-            0, 0, 0,
-            100, 0, 0,
-            100, 0, 30,
-            0, 0, 0,
-            100, 0, 30,
-            0, 0, 30,
-
-            // top rung right
-            100, 0, 0,
-            100, 30, 0,
-            100, 30, 30,
-            100, 0, 0,
-            100, 30, 30,
-            100, 0, 30,
-
-            // under top rung
-            30, 30, 0,
-            30, 30, 30,
-            100, 30, 30,
-            30, 30, 0,
-            100, 30, 30,
-            100, 30, 0,
-
-            // between top rung and middle
-            30, 30, 0,
-            30, 60, 30,
-            30, 30, 30,
-            30, 30, 0,
-            30, 60, 0,
-            30, 60, 30,
-
-            // top of middle rung
-            30, 60, 0,
-            67, 60, 30,
-            30, 60, 30,
-            30, 60, 0,
-            67, 60, 0,
-            67, 60, 30,
-
-            // right of middle rung
-            67, 60, 0,
-            67, 90, 30,
-            67, 60, 30,
-            67, 60, 0,
-            67, 90, 0,
-            67, 90, 30,
-
-            // bottom of middle rung.
-            30, 90, 0,
-            30, 90, 30,
-            67, 90, 30,
-            30, 90, 0,
-            67, 90, 30,
-            67, 90, 0,
-
-            // right of bottom
-            30, 90, 0,
-            30, 150, 30,
-            30, 90, 30,
-            30, 90, 0,
-            30, 150, 0,
-            30, 150, 30,
-
-            // bottom
-            0, 150, 0,
-            0, 150, 30,
-            30, 150, 30,
-            0, 150, 0,
-            30, 150, 30,
-            30, 150, 0,
-
-            // left side
-            0, 0, 0,
-            0, 0, 30,
-            0, 150, 30,
-            0, 0, 0,
-            0, 150, 30,
-            0, 150, 0,
-        ]),
+        fPixelArray,
         gl.STATIC_DRAW);
 }
 
@@ -652,6 +702,100 @@ var m4 = {
         return m4.multiply(m, m4.scaling(sx, sy, sz));
     },
 
+    inverse: function (m) {
+        var m00 = m[0 * 4 + 0];
+        var m01 = m[0 * 4 + 1];
+        var m02 = m[0 * 4 + 2];
+        var m03 = m[0 * 4 + 3];
+        var m10 = m[1 * 4 + 0];
+        var m11 = m[1 * 4 + 1];
+        var m12 = m[1 * 4 + 2];
+        var m13 = m[1 * 4 + 3];
+        var m20 = m[2 * 4 + 0];
+        var m21 = m[2 * 4 + 1];
+        var m22 = m[2 * 4 + 2];
+        var m23 = m[2 * 4 + 3];
+        var m30 = m[3 * 4 + 0];
+        var m31 = m[3 * 4 + 1];
+        var m32 = m[3 * 4 + 2];
+        var m33 = m[3 * 4 + 3];
+        var tmp_0 = m22 * m33;
+        var tmp_1 = m32 * m23;
+        var tmp_2 = m12 * m33;
+        var tmp_3 = m32 * m13;
+        var tmp_4 = m12 * m23;
+        var tmp_5 = m22 * m13;
+        var tmp_6 = m02 * m33;
+        var tmp_7 = m32 * m03;
+        var tmp_8 = m02 * m23;
+        var tmp_9 = m22 * m03;
+        var tmp_10 = m02 * m13;
+        var tmp_11 = m12 * m03;
+        var tmp_12 = m20 * m31;
+        var tmp_13 = m30 * m21;
+        var tmp_14 = m10 * m31;
+        var tmp_15 = m30 * m11;
+        var tmp_16 = m10 * m21;
+        var tmp_17 = m20 * m11;
+        var tmp_18 = m00 * m31;
+        var tmp_19 = m30 * m01;
+        var tmp_20 = m00 * m21;
+        var tmp_21 = m20 * m01;
+        var tmp_22 = m00 * m11;
+        var tmp_23 = m10 * m01;
+
+        var t0 = (tmp_0 * m11 + tmp_3 * m21 + tmp_4 * m31) -
+            (tmp_1 * m11 + tmp_2 * m21 + tmp_5 * m31);
+        var t1 = (tmp_1 * m01 + tmp_6 * m21 + tmp_9 * m31) -
+            (tmp_0 * m01 + tmp_7 * m21 + tmp_8 * m31);
+        var t2 = (tmp_2 * m01 + tmp_7 * m11 + tmp_10 * m31) -
+            (tmp_3 * m01 + tmp_6 * m11 + tmp_11 * m31);
+        var t3 = (tmp_5 * m01 + tmp_8 * m11 + tmp_11 * m21) -
+            (tmp_4 * m01 + tmp_9 * m11 + tmp_10 * m21);
+
+        var d = 1.0 / (m00 * t0 + m10 * t1 + m20 * t2 + m30 * t3);
+
+        return [
+            d * t0,
+            d * t1,
+            d * t2,
+            d * t3,
+            d * ((tmp_1 * m10 + tmp_2 * m20 + tmp_5 * m30) -
+                (tmp_0 * m10 + tmp_3 * m20 + tmp_4 * m30)),
+            d * ((tmp_0 * m00 + tmp_7 * m20 + tmp_8 * m30) -
+                (tmp_1 * m00 + tmp_6 * m20 + tmp_9 * m30)),
+            d * ((tmp_3 * m00 + tmp_6 * m10 + tmp_11 * m30) -
+                (tmp_2 * m00 + tmp_7 * m10 + tmp_10 * m30)),
+            d * ((tmp_4 * m00 + tmp_9 * m10 + tmp_10 * m20) -
+                (tmp_5 * m00 + tmp_8 * m10 + tmp_11 * m20)),
+            d * ((tmp_12 * m13 + tmp_15 * m23 + tmp_16 * m33) -
+                (tmp_13 * m13 + tmp_14 * m23 + tmp_17 * m33)),
+            d * ((tmp_13 * m03 + tmp_18 * m23 + tmp_21 * m33) -
+                (tmp_12 * m03 + tmp_19 * m23 + tmp_20 * m33)),
+            d * ((tmp_14 * m03 + tmp_19 * m13 + tmp_22 * m33) -
+                (tmp_15 * m03 + tmp_18 * m13 + tmp_23 * m33)),
+            d * ((tmp_17 * m03 + tmp_20 * m13 + tmp_23 * m23) -
+                (tmp_16 * m03 + tmp_21 * m13 + tmp_22 * m23)),
+            d * ((tmp_14 * m22 + tmp_17 * m32 + tmp_13 * m12) -
+                (tmp_16 * m32 + tmp_12 * m12 + tmp_15 * m22)),
+            d * ((tmp_20 * m32 + tmp_12 * m02 + tmp_19 * m22) -
+                (tmp_18 * m22 + tmp_21 * m32 + tmp_13 * m02)),
+            d * ((tmp_18 * m12 + tmp_23 * m32 + tmp_15 * m02) -
+                (tmp_22 * m32 + tmp_14 * m02 + tmp_19 * m12)),
+            d * ((tmp_22 * m22 + tmp_16 * m02 + tmp_21 * m12) -
+                (tmp_20 * m12 + tmp_23 * m22 + tmp_17 * m02)),
+        ];
+    },
+    transformVector: function (m, v) {
+        var dst = [];
+        for (var i = 0; i < 4; ++i) {
+            dst[i] = 0.0;
+            for (var j = 0; j < 4; ++j) {
+                dst[i] += v[j] * m[j * 4 + i];
+            }
+        }
+        return dst;
+    }
 };
 
 function updatePosition(index) {
