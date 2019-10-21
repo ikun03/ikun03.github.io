df37ec01f924ed557f602878eceaa22ec02ed924
Kunal <ikunal03@gmail.com>
2019-10-21 18:04:59 -0400

    Textures added

--- scripts/canvasScript2.js
+++ scripts/canvasScript2.js
@@ -6,37 +6,46 @@ let camera = new THREE.PerspectiveCamera(75, renderWidth / renderHeight, 0.1, 10
 let renderer = new THREE.WebGLRenderer();
 renderer.setSize(renderWidth, renderHeight);
 document.body.appendChild(renderer.domElement);
-let sphere = getSphere(0x00ff00);
-let sphere2 = getSphere(0xff0000);
-let sphere3 = getSphere(0x0000ff);
-let poolTable = getCube(0x226622);
+
+//Geometries here
+
+//Texture loading here
+//texture credits to robinwood.com and https://www.toptal.com/designers/subtlepatterns/pool-table/
+let cueTexture = new THREE.TextureLoader().load('../images/BallCue.jpg');
+let blueBallTexture = new THREE.TextureLoader().load('../images/Ball10.jpg');
+let redBallTexture = new THREE.TextureLoader().load('../images/Ball11.jpg');
+let greenBallTexture = new THREE.TextureLoader().load('../images/Ball14.jpg');
+let billiardsTableTexture = new THREE.TextureLoader().load('../images/pool_table.png');
+
+let blueBall = getSphere(blueBallTexture);
+let redBall = getSphere(redBallTexture);
+let greenBall = getSphere(greenBallTexture);
+let poolTable = getCube(billiardsTableTexture);
+let cueBall = getSphere(cueTexture);
 
 camera.position.z = 5;
-sphere.position.set(5, 0, -20);
-sphere2.position.set(0, 0, -20);
-sphere3.position.set(-5, 0, -20);
+blueBall.position.set(5, 10, -20);
+redBall.position.set(0, 15, -20);
+greenBall.position.set(-5, 10, -20);
 poolTable.position.set(0, 0, -21);
-scene.add(sphere, sphere2, sphere3, poolTable);
+cueBall.position.set(0, 0, -20);
+scene.add(blueBall, redBall, greenBall, poolTable, cueBall);
 
 var animate = function () {
     requestAnimationFrame(animate);
-
-    sphere.rotation.x += 0.01;
-    sphere.rotation.y += 0.01;
-
     renderer.render(scene, camera);
 };
 
 animate();
 
-function getCube(cubeColor) {
+function getCube(texture) {
     var geometry = new THREE.BoxGeometry(40, 40, 0.5);
-    var material = new THREE.MeshBasicMaterial({color: cubeColor});
+    var material = new THREE.MeshBasicMaterial({map: texture});
     return new THREE.Mesh(geometry, material);
 }
 
-function getSphere(sphereColor) {
+function getSphere(texture) {
     var geometry = new THREE.SphereGeometry(1, 20, 20);
-    var material = new THREE.MeshBasicMaterial({color: sphereColor});
+    var material = new THREE.MeshBasicMaterial({map: texture});
     return new THREE.Mesh(geometry, material);
 }
