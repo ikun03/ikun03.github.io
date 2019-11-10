fc4a50f6dae9ac5f0c226725fe5f931c5ef7dc0c
Kunal <ikunal03@gmail.com>
2019-11-09 22:02:59 -0500

    The basic outline of the data structure is pretty much set

--- scripts/classes/dcel.js
+++ scripts/classes/dcel.js
@@ -7,8 +7,26 @@ class DCEL {
         this.faces = new Map();
         this.halfedges = new Map();
     }
+
+    addVertex(vertex) {
+        //Find the face where the vertex is inserted
+        //Create and add the new halfedges to the list
+        //Then split the faces
+    }
+
+    flipEdges(oldHalfEdge, newHalfEdge) {
+        //Find the twin half edge and the faces of the half edge
+        //Now find the vertices of the new Half Edge
+        //Check if they are in the same faces as the original half edges
+        //Add their edges
+        //Remove old edges
+        //Split faces
+        //Update graph accordingly
+    }
+
 }
 
+//TODO: A lot of the data structures are just silly redundant so remove once done
 class Vertex {
     constructor(name, x, y) {
         this.vertexName = name;
@@ -20,16 +38,20 @@ class Vertex {
 
 class HalfEdge {
     constructor() {
+        this.edgeName = [];
         this.originVertex = null;
+        this.targetVertex = null;
         this.leftSideFace = null;
         this.twin = null;
+        this.nextHalfEdge = null;
+        this.previousHalfEdge = null;
     }
 }
 
 class Face {
     constructor() {
+        this.faceName = [];
         this.edge = null;
-
     }
 }
 
